47dd99600d38108d58326588bce988276b0fcd57
TeaPot98 <trifautan@gmail.com>
2021-12-27 11:25:27 +0200

    upd

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const Note = require('./models/note')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -12,19 +14,19 @@ app.use(express.json())
 //     id: 1,
 //     content: "HTML is easy",
 //     date: "2020-01-10T17:30:31.098Z",
-//     important: true
+//     imconfig.portant: true
 //   },
 //   {
 //     id: 2,
 //     content: "Browser can execute only Javascript",
 //     date: "2020-01-10T18:39:34.091Z",
-//     important: false
+//     imconfig.portant: false
 //   },
 //   {
 //     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
+//     content: "GET and POST are the most imconfig.portant methods of HTTP protocol",
 //     date: "2020-01-10T19:20:14.298Z",
-//     important: true
+//     imconfig.portant: true
 //   }
 // ]
 
@@ -119,9 +121,8 @@ app.delete('/api/notes/:id', (request, response, next) => {
 		.catch(error => next(error))
 })
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+	logger.info(`Server running on config.port ${config.PORT}`)
 })
 
 const errorHandler = (error, request, response, next) => {
